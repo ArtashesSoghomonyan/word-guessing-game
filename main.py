fb74beb7facaea975576cfe2fc5d2317967ef517
Artashes Soghomonyan <artashessoghomonyan@gmail.com>
2026-02-26 00:05:45 +0400

    get user input

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import sys
+
+ALPHABET = list("abcdefghijklmnopqrstuvwxyz")
+
 def main():
     print("Word guessing game!")
+
+    try:
+        guess = input("Enter your guess: ")
+
+        if guess.lower() not in ALPHABET:
+            print(f"Invalid input: {guess}")
+        else:
+            print(f"Your guess is {guess}")
+
+    except KeyboardInterrupt:
+        print("\nYou have interrupted the game.")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
