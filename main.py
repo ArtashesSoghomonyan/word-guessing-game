4af54666cdecedfffc00144357460c8abb1e724e
Artashes Soghomonyan <artashessoghomonyan@gmail.com>
2026-02-26 15:16:27 +0400

    user input validation

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import sys
+import constants
 
 ALPHABET = list("abcdefghijklmnopqrstuvwxyz")
 
-def main():
-    print("Word guessing game!")
-
+def get_user_input():
     try:
         guess = input("Enter your guess: ")
 
         if guess.lower() not in ALPHABET:
-            print(f"Invalid input: {guess}")
+            return None
         else:
-            print(f"Your guess is {guess}")
+            return guess
 
     except KeyboardInterrupt:
         print("\nYou have interrupted the game.")
         sys.exit(1)
 
+
+def main():
+    print("Word guessing game!")
+    
+    while True:
+        guess = get_user_input()
+        
+        if guess == None:
+            print("Invalid input!")
+        else:
+            print(guess)
+    
 if __name__ == "__main__":
     main()
